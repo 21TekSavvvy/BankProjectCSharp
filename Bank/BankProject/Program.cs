198ac44362a9967b9fefb51aa1fd33d7f60312a7
21TekSavvvy <jareddarochajj@gmail.com>
2022-02-10 21:54:33 -0800

    Made login method instead all in the Main

--- Bank/BankProject/Program.cs
+++ Bank/BankProject/Program.cs
@@ -2,25 +2,31 @@
 {
     static void Main ()
     {
+        Bank bank = new Bank ();
+        bank.login();
+        System.Console.WriteLine ("Thank you Visit Again");
+        System.Console.ReadKey ();
+    }
+
+    public void login()
+    {
         System.Console.WriteLine("*******************Bank******************");
         System.Console.WriteLine("::Login Page::");
 
         string userName = null, password = null;
 
         System.Console.Write("Username: ");
-        userName = System.Console.ReadLine ();
+        userName = System.Console.ReadLine();
 
         if (userName != "")
         {
             System.Console.Write("Password: ");
-            password = System.Console.ReadLine ();
+            password = System.Console.ReadLine();
         }
 
-        if(userName =="system" && password == "manager")
+        if (userName == "system" && password == "manager")
         {
             System.Console.WriteLine("To Do: Main menu here");
         }
-        System.Console.WriteLine ("Thank you Visit Again");
-
     }
 }
