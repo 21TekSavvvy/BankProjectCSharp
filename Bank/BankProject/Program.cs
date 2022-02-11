2f317e1469291ab87c2bbd8bd373b481fe297c37
21TekSavvvy <jareddarochajj@gmail.com>
2022-02-10 17:30:11 -0800

    Login Page created

---
+++ Bank/BankProject/Program.cs
@@ -0,0 +1,26 @@
+﻿class Bank
+{
+    static void Main ()
+    {
+        System.Console.WriteLine("*******************Bank******************");
+        System.Console.WriteLine("::Login Page::");
+
+        string userName = null, password = null;
+
+        System.Console.Write("Username: ");
+        userName = System.Console.ReadLine ();
+
+        if (userName != "")
+        {
+            System.Console.Write("Password: ");
+            password = System.Console.ReadLine ();
+        }
+
+        if(userName =="system" && password == "manager")
+        {
+            System.Console.WriteLine("To Do: Main menu here");
+        }
+        System.Console.WriteLine ("Thank you Visit Again");
+
+    }
+}
