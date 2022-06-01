2ca21d65498a7801101cd6f2f7a960567569ce67
21TekSavvvy <jareddarochajj@gmail.com>
2022-05-31 20:44:25 -0700

    Finished DAL, started exception handling in DAL

--- Bank/Bank.DataAccessLayer/CustomerDataAccessLayer.cs
+++ Bank/Bank.DataAccessLayer/CustomerDataAccessLayer.cs
@@ -47,8 +47,108 @@ namespace Bank.DataAccessLayer
              Customers.ForEach(item =>customersList.Add(item.Clone() as Customer));
             return customersList;
         }
-        public List<Customer> GetCustomersByCondition()
+
+        /// <summary>
+        /// Returns List of customers that are matching with specified criteria
+        /// </summary>
+        /// <param name="predicate">Lambda expression with expression</param>
+        /// <returns>List of matching customers</returns>
+        public List<Customer> GetCustomersByCondition(Predicate<Customer> predicate)
         {
+            //create a new customer list
+            List<Customer> customersList = new List<Customer>();
+            // filter the collection
+            List<Customer> filteredCustomer = Customers.FindAll(predicate);
+            // copy all customer from the source collection into the new Customers list
+            filteredCustomer.ForEach(item => customersList.Add(item.Clone() as Customer));
+            return customersList;
+        }
+        /// <summary>
+        /// Adds a new customer to the existing list
+        /// </summary>
+        /// <param name="customer">Customer object to add</param>
+        /// <returns>Returns Guid of a newly created customer</returns>
+        public Guid AddCustomer(Customer customer)
+        {
+            // generate new guid
+            customer.CustomerID = Guid.NewGuid();
+            //add customer
+            Customers.Add(customer);
+            return customer.CustomerID;
+        }
+        /// <summary>
+        /// Updates an existing customer's detail
+        /// </summary>
+        /// <param name="c">Customer object with updated details</param>
+        /// <returns>Determines whether the customer is updated or not</returns>
+        public bool UpdateCustomer(Customer c)
+        {
+            try
+            {
+                // find existing customer by CustomerID
+                Customer existingCustomer = Customers.Find(item => item.CustomerID == c.CustomerID);
+
+                // update all details of customer
+                if (existingCustomer != null)
+                {
+                    existingCustomer.CustomerCode = c.CustomerCode;
+                    existingCustomer.CustomerName = c.CustomerName;
+                    existingCustomer.Address = c.Address;
+                    existingCustomer.City = c.City;
+                    existingCustomer.Mobile = c.Mobile;
+                    existingCustomer.Landmark = c.Landmark;
+                    existingCustomer.Country = c.Country;
+
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (CustomerException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+           
+
+        }
+
+        /// <summary>
+        /// Deletes an existing customer based on customerID
+        /// </summary>
+        /// <param name="cID">CustomerID to delete</param>
+        /// <returns>Indicates whether the Customer is delted or not</returns>
+        public bool DeleteCustomer(Guid cID)
+        {
+            try
+            {
+                //delete customer by customerID
+                if (Customers.RemoveAll(item => item.CustomerID == cID) > 0)
+                {
+                    return true; // indicates one or more customers are deleted
+                }
+                else
+                {
+                    return false; // indicates no customer is deleted
+                }
+            }
+            catch (CustomerException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+          
 
         }
         #endregion
